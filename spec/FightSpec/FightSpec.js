5a8dc51e9028af53090cb24fdde33ba331519598
Louis Vanier <vanier.louis@gmail.com>
2016-04-02 20:01:37 -0400

    Fix module name export

--- spec/FightSpec/FightSpec.js
+++ spec/FightSpec/FightSpec.js
@@ -1,5 +1,5 @@
 describe("FightModel", function() {
-  var FightModel = require('../../ViewModel');
+  var FightModel = require('../../ViewModel/FightModel');
   var Character = require('../../ViewModel/Character');
   var TrackableEffect = require('../../ViewModel/TrackableEffect');
   var ko = require('../../knockout/ko');
