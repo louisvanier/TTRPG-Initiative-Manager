50d9b2c48789997236db7f78205d615ecb2e0ccd
Louis Vanier <louis.vanier@shopify.com>
2016-09-22 08:52:00 -0400

    finish trackableEffects tests

--- test/javascripts/unit/trackableEffectTest.js
+++ test/javascripts/unit/trackableEffectTest.js
@@ -22,30 +22,69 @@ describe("parseEffectType", () => {
 });
 
 describe("trackableEffect model", () => {
-  let trackableEffect 
+  let trackableEffect = null;
   beforeEach(() => {
-    //instantiate a new model
+    trackableEffect = trackableEffectModel({});
   });
 
   describe("update model data", () => {
     beforeEach(() => {
-      //reset the models data
+      trackableEffect.update({});
     });
 
     it("should properly set values for title, description, duration and effectType", () => {
-    
+      let updateData = {
+        title: 'Bless',
+        description: 'Buff +1 morale bonus on attack rolls and saves vs fear effects',
+        duration: 3,
+        effectType: 'Neutral'
+      };
+
+      trackableEffect.update(updateData);
+      expect(trackableEffect.title()).toEqual('Bless');
+      expect(trackableEffect.description()).toEqual('Buff +1 morale bonus on attack rolls and saves vs fear effects');
+      expect(trackableEffect.duration()).toEqual(3);
+      expect(trackableEffect.effectType()).toEqual(effectTypeNeutral);
     });
     it("should default title to 'New effect'", () => {
-    
+      let updateData = {
+        description: 'description but I forgot a title',
+        duration: 3,
+        effectType: 'Neutral'
+      };
+
+      trackableEffect.update(updateData);
+      expect(trackableEffect.title()).toEqual('New effect');
     });
     it("should default description to an empty string", () => {
-    
+      let updateData = {
+        title: "Bull's Strength",
+        duration: 3,
+        effectType: 'Neutral'
+      };
+
+      trackableEffect.update(updateData);
+      expect(trackableEffect.duration()).toEqual('');
     });
     it("should default duration to -1", () => {
-    
+      let updateData = {
+        title: "Bull's Strength",
+        description: '+4 enhancement bonus to strength',
+        effectType: 'Neutral'
+      };
+
+      trackableEffect.update(updateData);
+      expect(trackableEffect.duration()).toEqual(-1);
     });
     it("should default effectType to Beneficial", () => {
-    
+      let updateData = {
+        title: "Bull's Strength",
+        description: '+4 enhancement bonus to strength',
+        duration: 3
+      };
+
+      trackableEffect.update(updateData);
+      expect(trackableEffect.effectType()).toEqual(effectTypeBeneficial);
     });
   })
 });
