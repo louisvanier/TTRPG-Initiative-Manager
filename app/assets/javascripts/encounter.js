b56086853237a78b58c7b9d7d54b0a1e7302eb39
Louis Vanier <louis.vanier@shopify.com>
2016-10-03 22:50:44 -0400

    fix encounter test and add findCharacter

--- app/assets/javascripts/encounter.js
+++ app/assets/javascripts/encounter.js
@@ -1,4 +1,6 @@
 let ko = require('knockout')
+let TrackableEffectModel = require('trackableEffect.js').trackableEffect;
+let CharacterModel = require('character.js').character;
 
 let EncounterModel = class {
   constructor() {
@@ -25,15 +27,28 @@ let EncounterModel = class {
     this.selectedEffect = ko.observable();
     //when adding an effect
     this.temporaryEffect = ko.observable();
-
-
   }
 
   update(data) {
     this.currentRound(data.currentRound || 1);
     this.rankInCurrentRound(data.rankInCurrentRound || 1);
-    this.characters(data.characters || []);
-    this.effects(data.effects || []);
+
+    (data.characters || []).forEach((characterData) => {
+      let character = new CharacterModel();
+      character.update(characterData);
+      this.characters.push(character);
+    }, this);
+    (data.effects || []).forEach((effectData) => {
+      let effect = new TrackableEffectModel();
+      effect.update(effectData);
+      this.effects.push(effect);
+    }, this);
+  }
+
+  findCharacter(characterName) {
+    return ko.utils.arrayFirst(this.characters(), (character) => {
+        return character.name() === characterName;
+    });
   }
 }
 
--- spec/javascripts/encounter_spec.js
+++ spec/javascripts/encounter_spec.js
@@ -4,7 +4,7 @@ describe("EncounterModel", () => {
   let encounter = null;
   let modelData = null;
   beforeEach(() => {
-    encounter = new Encounter({});
+    encounter = new EncounterModel({});
     modelData = {
         currentRound: 5,
         rankInCurrentRound: 2,
@@ -20,7 +20,7 @@ describe("EncounterModel", () => {
           }
         ],
         effects: [{
-          title: 'Blood for the blood god'
+          title: 'Blood for the blood god',
           description: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
           duration: 4,
           effectType: 'Beneficial'
@@ -33,11 +33,11 @@ describe("EncounterModel", () => {
       encounter.update(modelData);
       expect(encounter.currentRound()).toEqual(5);
       expect(encounter.rankInCurrentRound()).toEqual(2);
-      expect(encounter.characters.length).toEqual(2);
+      expect(encounter.characters().length).toEqual(2);
       expect(encounter.characters()[0].name()).toEqual('Jimmay');
       expect(encounter.characters()[1].name()).toEqual('Timmay');
-      expect(encounter.efects.length).toEqual(1);
-      expect(encounter.efects()[0].title()).toEqual('Blood for the blood god');
+      expect(encounter.effects().length).toEqual(1);
+      expect(encounter.effects()[0].title()).toEqual('Blood for the blood god');
     });
     it("should default currentRound to 1", () => {
       delete modelData.currentRound;
@@ -64,6 +64,20 @@ describe("EncounterModel", () => {
       expect(typeof modelData.effects).toEqual('undefined');
       
       expect(encounter.characters.length).toEqual(0);
+    });
+  });
+
+  describe("findCharacter", () => {
+    let jimmay = null;
+    beforeEach(() => {
+      encounter.update(modelData);
+      jimmay = encounter.characters()[0];
+    });
+    it("should return a character by its name", () => {
+      expect(encounter.findCharacter('Jimmay')).toEqual(jimmay);
+    });
+    it("should return undefined when it cannot find the character by its name", () => {
+      expect(encounter.findCharacter('Yoda great master')).toEqual(null);
     });
   });
 });
