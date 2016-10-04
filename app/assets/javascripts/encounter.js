86eedfbd2447c88610fce3d9c80fdb95bb781560
Louis Vanier <louis.vanier@shopify.com>
2016-10-03 21:47:58 -0400

    encounter model

---
+++ app/assets/javascripts/encounter.js
@@ -0,0 +1,30 @@
+let ko = require('knockout')
+
+let EncounterModel = class {
+  constructor() {
+    //basic info
+    this.currentRound = ko.observable();
+    this.rankInCurrentRound = ko.observable();
+
+    this.characters = ko.observableArray();
+    //when editing a character
+    this.selectedCharacter = ko.observable();
+    //when adding a character
+    this.temporaryCharacter = ko.observable();
+
+    this.effects = ko.observableArray();
+    //when editing an effect
+    this.selectedEffect = ko.observable();
+    //when adding an effect
+    this.temporaryEffect = ko.observable();
+  }
+
+  update(data) {
+    this.currentRound(data.currentRound || 1);
+    this.rankInCurrentRound(data.rankInCurrentRound || 1);
+    this.characters(data.characters || []);
+    this.effects(data.effects || []);
+  }
+}
+
+exports.encounter = EncounterModel
---
+++ spec/javascripts/encounter_spec.js
@@ -0,0 +1,69 @@
+let EncounterModel = require('../../app/assets/javascripts/encounter.js').encounter;
+
+describe("EncounterModel", () => {
+  let encounter = null;
+  let modelData = null;
+  beforeEach(() => {
+    encounter = new Encounter({});
+    modelData = {
+        currentRound: 5,
+        rankInCurrentRound: 2,
+        characters: [{ 
+          name: 'Jimmay',
+          rankInCombat: 1,
+          status: "ALREADY_ACTED"
+          },
+          { 
+          name: 'Timmay',
+          rankInCombat: 2,
+          status: "CURRENTLY_ACTING"
+          }
+        ],
+        effects: [{
+          title: 'Blood for the blood god'
+          description: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
+          duration: 4,
+          effectType: 'Beneficial'
+        }]
+      };
+  });
+
+  describe("Update", () => {
+    it("should set currentRound, rankInCurrentRound, characters and effects", () => {
+      encounter.update(modelData);
+      expect(encounter.currentRound()).toEqual(5);
+      expect(encounter.rankInCurrentRound()).toEqual(2);
+      expect(encounter.characters.length).toEqual(2);
+      expect(encounter.characters()[0].name()).toEqual('Jimmay');
+      expect(encounter.characters()[1].name()).toEqual('Timmay');
+      expect(encounter.efects.length).toEqual(1);
+      expect(encounter.efects()[0].title()).toEqual('Blood for the blood god');
+    });
+    it("should default currentRound to 1", () => {
+      delete modelData.currentRound;
+      expect(typeof modelData.currentRound).toEqual('undefined');
+
+      encounter.update(modelData);
+      expect(encounter.currentRound()).toEqual(1);
+    });
+    it("should default rankInCurrentRound to 1", () => {
+      delete modelData.rankInCurrentRound;
+      expect(typeof modelData.rankInCurrentRound).toEqual('undefined');
+
+      encounter.update(modelData);
+      expect(encounter.rankInCurrentRound()).toEqual(1);
+    });
+    it("should default characters to an empty array", () => {
+      delete modelData.characters;
+      expect(typeof modelData.characters).toEqual('undefined');
+
+      expect(encounter.characters.length).toEqual(0);
+    });
+    it("should default effects to an empty array", () => {
+      delete modelData.effects;
+      expect(typeof modelData.effects).toEqual('undefined');
+      
+      expect(encounter.characters.length).toEqual(0);
+    });
+  });
+});
