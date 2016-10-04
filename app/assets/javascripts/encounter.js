4f435ac31cb6119bf86a4b37af3ea8188d79f122
Louis Vanier <louis.vanier@shopify.com>
2016-10-03 22:17:59 -0400

    stinky comments for huge encounter model disposition

--- app/assets/javascripts/encounter.js
+++ app/assets/javascripts/encounter.js
@@ -2,21 +2,31 @@ let ko = require('knockout')
 
 let EncounterModel = class {
   constructor() {
+    //--------------
     //basic info
+    //--------------
     this.currentRound = ko.observable();
     this.rankInCurrentRound = ko.observable();
 
+    //--------------
+    //characters
+    //--------------
     this.characters = ko.observableArray();
     //when editing a character
     this.selectedCharacter = ko.observable();
     //when adding a character
     this.temporaryCharacter = ko.observable();
 
+    //--------------
+    //effects
+    //--------------
     this.effects = ko.observableArray();
     //when editing an effect
     this.selectedEffect = ko.observable();
     //when adding an effect
     this.temporaryEffect = ko.observable();
+
+
   }
 
   update(data) {
